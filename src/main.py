0b44474b178dd31c50acb11e56bd541e969dd670
EylonNaamat <eylonnaamat@gmail.com>
2023-01-15 11:51:52 +0200

    modified signup

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ def read_root():
     return {"Hello": "World"}
 
 @app.get("/signup/user/checkuserexists")
-async def get_user(username:str):
+async def checkuserexists(username:str):
     try:
         ref = db.reference('users')
         user_ref = ref.child(username)
@@ -29,7 +29,7 @@ async def get_user(username:str):
     return {"user": "doesnt exists"}
 
 @app.get("/signup/user/insertuser")
-async def get_user(first_name:str, last_name:str, email:str, username:str, password:str, city:str, birth_date:str, gender:str, is_manager:str, super_id:str):
+async def insertuser(first_name:str, last_name:str, email:str, username:str, password:str, city:str, birth_date:str, gender:str, is_manager:str, super_id:str):
     PARAMS = {'first_name':first_name, 'last_name':last_name, 'email':email, 'username':username,
             'password':password, 'city':city, 'birth_date':birth_date, 'gender':gender, 'is_manager':is_manager,
             'super_id':super_id}
@@ -43,8 +43,9 @@ async def get_user(first_name:str, last_name:str, email:str, username:str, passw
     return {"user": "success"}
 
 @app.get("/signup/super/insertsuper")
-async def get_user(super_ID:str, super_name:str, super_city:str):
-    PARAMS = {'super_ID':super_ID, 'super_name':super_name, 'super_city':super_city}
+async def insertsuper(super_ID:str, super_name:str, super_city:str, comments_size:str, super_rating:str):
+    PARAMS = {'super_ID':super_ID, 'super_name':super_name, 'super_city':super_city, 
+            'comments_size':comments_size, 'super_rating':super_rating}
     try:
         ref = db.reference('Supers')
         user_ref = ref.child(super_ID)
@@ -56,7 +57,7 @@ async def get_user(super_ID:str, super_name:str, super_city:str):
 
 
 @app.get("/signup/city/insertcity")
-async def get_user(super_ID:str, super_name:str, super_city:str):
+async def insertcity(super_ID:str, super_city:str):
     PARAMS = {"active":True}
     try:
         ref = db.reference('cities')
@@ -83,7 +84,6 @@ async def get_user(first_name:str, last_name:str, email:str, username:str, passw
         ref = db.reference('users')
         user_ref = ref.child(username)
         user_ref.update(PARAMS)
-        # ref = db.reference(f"users/{username}/first_name")
     except:
         return {"Hello": 'error'}
 
